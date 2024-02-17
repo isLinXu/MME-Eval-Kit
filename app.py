e1dd80795c31b53369f1448142c6fa65a18fe83c
isLinXu <2267379130@qq.com>
2024-02-18 00:17:57 +0800

    update app.py

--- app.py
+++ app.py
@@ -1,34 +1,3 @@
-#
-#
-# import gradio as gr
-# from utils.plots import plot_evaluation_chart, plot_histogram
-#
-# def create_interface():
-#     with gr.Blocks() as demo:
-#         data_frame, output_path = plot_evaluation_chart("/Users/gatilin/PycharmProjects/MME-Eval-Kit/data/mllm_acc_eval-csv_private_0128.csv","Existence")
-#         with gr.TabItem('🔍 About', elem_id='about', id=1):
-#             gr.Markdown("## MME-Eval-Kit")
-#             gr.Markdown("MME-Eval-Kit is a toolkit for model metrics evaluation.")
-#             gr.Image(output_path, label="Evaluation Chart",show_label=True, width=480, height=480)
-#         with gr.Tabs(elem_classes='tab-buttons') as tabs:
-#             # 创建标签页
-#             for column in data_frame.columns:
-#                 if type(data_frame[column].values[0]) != str:
-#                     with gr.TabItem(f'📊 {column} Leaderboard', elem_id=column):
-#                         # 在每个标签页中展示对应列的数据
-#                         gr.Markdown(f"## {column} Leaderboard")
-#                         sorted_data_frame = data_frame.sort_values(column, ascending=False)
-#                         gr.components.DataFrame(value=sorted_data_frame, type="pandas", interactive=False, visible=True)
-#
-#                         # 在每个标签页中显示对应排序的直方图
-#                         histogram_path = f"histogram_{column}.png"
-#                         plot_histogram(data_frame, column, output_path=histogram_path)
-#                         gr.Image(histogram_path, label=f"{column} Histogram",show_label=True, width=640, height=480)
-#     demo.launch()
-#
-# if __name__ == '__main__':
-#     create_interface()
-
 import gradio as gr
 
 from utils.files import save_image_to_base64
@@ -38,8 +7,7 @@ import base64
 
 def create_interface():
     with gr.Blocks() as demo:
-        data_frame, output_path = plot_evaluation_chart(
-            "/Users/gatilin/PycharmProjects/MME-Eval-Kit/data/mllm_acc_eval-csv_private_0128.csv", "Existence")
+        data_frame, output_path = plot_evaluation_chart("/Users/gatilin/PycharmProjects/MME-Eval-Kit/data/mllm_acc_eval-csv_private_0128.csv", "Existence")
         with gr.TabItem('🔍 About', elem_id='about', id=1):
             gr.Markdown("## MME-Eval-Kit")
             gr.Markdown("MME-Eval-Kit is a toolkit for model metrics evaluation.")
