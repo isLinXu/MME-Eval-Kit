0b3c1b62e38db0649f64cfd35adc1227f238696b
isLinXu <2267379130@qq.com>
2024-02-18 00:51:34 +0800

    update app.py

--- app.py
+++ app.py
@@ -1,13 +1,84 @@
+# import gradio as gr
+#
+# from utils.files import save_image_to_base64
+# from utils.plots import plot_evaluation_chart, plot_histogram
+# import base64
+#
+# def calculate_perception(row):
+#     columns = ['Existence','Count','Position','Color','Poster','Celebrity','Scene','Landmark','Artwork','OCR']
+#     count = sum(row[column] for column in columns)
+#     return round(count,2)
+#
+# def calculate_cognition(row):
+#     columns = ['Commensense Reasoning','Numerical Calculation','Text Translation','Code Reasoning']
+#     count = sum(row[column] for column in columns)
+#     return round(count,2)
+# def create_interface():
+#     with gr.Blocks() as demo:
+#         data_frame, output_path = plot_evaluation_chart("/Users/gatilin/PycharmProjects/MME-Eval-Kit/data/mllm_acc_eval-csv_private_0128.csv", "Existence")
+#         data_frame['Perception'] = data_frame.apply(calculate_perception, axis=1)
+#         data_frame['Cognition'] = data_frame.apply(calculate_cognition, axis=1)
+#         with gr.TabItem('🔍 About', elem_id='about', id=1):
+#             gr.Markdown("## MME-Eval-Kit")
+#             gr.Markdown("MME-Eval-Kit is a toolkit for model metrics evaluation.")
+#
+#             # 将图像转换为base64编码
+#             encoded_leida_string = save_image_to_base64(output_path)
+#             gr.Markdown(f'<center><img src="data:image/png;base64,{encoded_leida_string}" alt="Evaluation Chart" width=480 height=480></center>')
+#         with gr.Tabs(elem_classes='tab-buttons') as tabs:
+#             # 创建标签页
+#             with gr.TabItem(f'📊 Perception Leaderboard'):
+#                 # 在每个标签页中展示对应列的数据
+#                 gr.Markdown(f"## Perception Leaderboard")
+#                 sorted_data_frame = data_frame.sort_values('Perception', ascending=False)
+#                 gr.components.DataFrame(value=sorted_data_frame, type="pandas", interactive=False, visible=True)
+#
+#             with gr.TabItem(f'📊 Cognition Leaderboard'):
+#                 # 在每个标签页中展示对应列的数据
+#                 gr.Markdown(f"## Cognition Leaderboard")
+#                 sorted_data_frame = data_frame.sort_values('Cognition', ascending=False)
+#                 gr.components.DataFrame(value=sorted_data_frame, type="pandas", interactive=False, visible=True)
+#
+#             for column in data_frame.columns:
+#                 if type(data_frame[column].values[0]) != str:
+#                     with gr.TabItem(f'📊 {column} Leaderboard', elem_id=column):
+#                         # 在每个标签页中展示对应列的数据
+#                         gr.Markdown(f"## {column} Leaderboard")
+#                         sorted_data_frame = data_frame.sort_values(column, ascending=False)
+#                         gr.components.DataFrame(value=sorted_data_frame, type="pandas", interactive=False, visible=True)
+#
+#                         # 在每个标签页中显示对应排序的直方图
+#                         histogram_path = f"histogram_{column}.png"
+#                         plot_histogram(data_frame, column, output_path=histogram_path)
+#                         encoded_histogram_string = save_image_to_base64(histogram_path)
+#                         gr.Markdown(f'<center><img src="data:image/png;base64,{encoded_histogram_string}" alt="f"{column} Histogram" width=640 height=480></center>')
+#     demo.launch()
+#
+#
+# if __name__ == '__main__':
+#     create_interface()
+
 import gradio as gr
 
 from utils.files import save_image_to_base64
 from utils.plots import plot_evaluation_chart, plot_histogram
 import base64
 
+def calculate_perception(row):
+    columns = ['Existence','Count','Position','Color','Poster','Celebrity','Scene','Landmark','Artwork','OCR']
+    count = sum(row[column] for column in columns)
+    return round(count,2)
+
+def calculate_cognition(row):
+    columns = ['Commensense Reasoning','Numerical Calculation','Text Translation','Code Reasoning']
+    count = sum(row[column] for column in columns)
+    return round(count,2)
 
 def create_interface():
     with gr.Blocks() as demo:
         data_frame, output_path = plot_evaluation_chart("/Users/gatilin/PycharmProjects/MME-Eval-Kit/data/mllm_acc_eval-csv_private_0128.csv", "Existence")
+        data_frame['Perception'] = data_frame.apply(calculate_perception, axis=1)
+        data_frame['Cognition'] = data_frame.apply(calculate_cognition, axis=1)
         with gr.TabItem('🔍 About', elem_id='about', id=1):
             gr.Markdown("## MME-Eval-Kit")
             gr.Markdown("MME-Eval-Kit is a toolkit for model metrics evaluation.")
@@ -17,6 +88,30 @@ def create_interface():
             gr.Markdown(f'<center><img src="data:image/png;base64,{encoded_leida_string}" alt="Evaluation Chart" width=480 height=480></center>')
         with gr.Tabs(elem_classes='tab-buttons') as tabs:
             # 创建标签页
+            # with gr.TabItem(f'📊 Perception Leaderboard'):
+            #     # 在每个标签页中展示对应列的数据
+            #     gr.Markdown(f"## Perception Leaderboard")
+            #     sorted_data_frame = data_frame.sort_values('Perception', ascending=False)
+            #     gr.components.DataFrame(value=sorted_data_frame, type="pandas", interactive=False, visible=True)
+            #
+            #     # 在Perception标签页中显示直方图
+            #     histogram_path = f"histogram_Perception.png"
+            #     plot_histogram(data_frame, 'Perception', output_path=histogram_path)
+            #     encoded_histogram_string = save_image_to_base64(histogram_path)
+            #     gr.Markdown(f'<center><img src="data:image/png;base64,{encoded_histogram_string}" alt="Perception Histogram" width=640 height=480></center>')
+            #
+            # with gr.TabItem(f'📊 Cognition Leaderboard'):
+            #     # 在每个标签页中展示对应列的数据
+            #     gr.Markdown(f"## Cognition Leaderboard")
+            #     sorted_data_frame = data_frame.sort_values('Cognition', ascending=False)
+            #     gr.components.DataFrame(value=sorted_data_frame, type="pandas", interactive=False, visible=True)
+            #
+            #     # 在Cognition标签页中显示直方图
+            #     histogram_path = f"histogram_Cognition.png"
+            #     plot_histogram(data_frame, 'Cognition', output_path=histogram_path)
+            #     encoded_histogram_string = save_image_to_base64(histogram_path)
+            #     gr.Markdown(f'<center><img src="data:image/png;base64,{encoded_histogram_string}" alt="Cognition Histogram" width=640 height=480></center>')
+
             for column in data_frame.columns:
                 if type(data_frame[column].values[0]) != str:
                     with gr.TabItem(f'📊 {column} Leaderboard', elem_id=column):
